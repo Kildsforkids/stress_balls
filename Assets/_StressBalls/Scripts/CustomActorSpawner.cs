0947144d3ae97e3a7646f253e9748b590dfe46dd
Igor Silchenko <kildsforkids@gmail.com>
2022-12-05 18:37:14 +1000

    Added StressBalls namespace

--- Assets/_StressBalls/Scripts/CustomActorSpawner.cs
+++ Assets/_StressBalls/Scripts/CustomActorSpawner.cs
@@ -1,32 +1,34 @@
 using Obi;
 using UnityEngine;
 
-public class CustomActorSpawner : MonoBehaviour {
+namespace StressBalls {
+    public class CustomActorSpawner : MonoBehaviour {
 
-    [SerializeField] private ObiActor template;
+        [SerializeField] private ObiActor template;
 
-    [SerializeField] private int basePhase = 2;
-    [SerializeField] private int maxInstances = 32;
-    [SerializeField] private float spawnDelay = 0.3f;
+        [SerializeField] private int basePhase = 2;
+        [SerializeField] private int maxInstances = 32;
+        [SerializeField] private float spawnDelay = 0.3f;
 
-    private int _phase = 0;
-    private int _instances = 0;
-    private float _timeFromLastSpawn = 0;
+        private int _phase = 0;
+        private int _instances = 0;
+        private float _timeFromLastSpawn = 0;
 
-    void Update() {
+        void Update() {
 
-        _timeFromLastSpawn += Time.deltaTime;
+            _timeFromLastSpawn += Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.Space) && _instances < maxInstances && _timeFromLastSpawn > spawnDelay) {
-            SpawnActor();
+            if (Input.GetKeyDown(KeyCode.Space) && _instances < maxInstances && _timeFromLastSpawn > spawnDelay) {
+                SpawnActor();
+            }
         }
-    }
-    
-    private void SpawnActor() {
-        Instantiate(template.gameObject, transform.position, transform.rotation, transform.parent)
-            .GetComponent<ObiActor>().SetFilterCategory(basePhase + _phase);
-        _phase++;
-        _instances++;
-        _timeFromLastSpawn = 0;
+
+        private void SpawnActor() {
+            Instantiate(template.gameObject, transform.position, transform.rotation, transform.parent)
+                .GetComponent<ObiActor>().SetFilterCategory(basePhase + _phase);
+            _phase++;
+            _instances++;
+            _timeFromLastSpawn = 0;
+        }
     }
 }
--- Assets/_StressBalls/Scripts/OrbitCamera.cs
+++ Assets/_StressBalls/Scripts/OrbitCamera.cs
@@ -1,50 +1,52 @@
 using UnityEngine;
 
-public class OrbitCamera : MonoBehaviour {
+namespace StressBalls {
+    public class OrbitCamera : MonoBehaviour {
 
-    [SerializeField] private Camera camera;
-    [SerializeField] private Transform target;
-    [SerializeField] private float distance = 10f;
-    [SerializeField] private MouseButton mouseButton;
+        [SerializeField] private Camera camera;
+        [SerializeField] private Transform target;
+        [SerializeField] private float distance = 10f;
+        [SerializeField] private MouseButton mouseButton;
 
-    private Vector3 _previousPosition;
-    private int _mouseButton;
+        private Vector3 _previousPosition;
+        private int _mouseButton;
 
-    private enum MouseButton {
-        Left,
-        Right,
-        Middle
-    }
-
-    private void Start() {
-        _mouseButton = (int)mouseButton;
-    }
-
-    private void Update() {
-        if (Input.GetMouseButtonDown(_mouseButton)) {
-            UpdatePreviousPosition(GetMousePoint());
+        private enum MouseButton {
+            Left,
+            Right,
+            Middle
         }
 
-        if (Input.GetMouseButton(_mouseButton)) {
-            Vector3 mousePoint = GetMousePoint();
-            Vector3 direction = (_previousPosition - mousePoint).normalized;
-
-            RotateCamera(direction);
-
-            UpdatePreviousPosition(mousePoint);
+        private void Start() {
+            _mouseButton = (int)mouseButton;
         }
+
+        private void Update() {
+            if (Input.GetMouseButtonDown(_mouseButton)) {
+                UpdatePreviousPosition(GetMousePoint());
+            }
+
+            if (Input.GetMouseButton(_mouseButton)) {
+                Vector3 mousePoint = GetMousePoint();
+                Vector3 direction = (_previousPosition - mousePoint).normalized;
+
+                RotateCamera(direction);
+
+                UpdatePreviousPosition(mousePoint);
+            }
+        }
+
+        private void RotateCamera(Vector3 direction) {
+            camera.transform.position = target.position;
+            camera.transform.Rotate(Vector3.right, direction.y * 180f * Time.deltaTime);
+            camera.transform.Rotate(Vector3.up, -direction.x * 180f * Time.deltaTime, Space.World);
+            camera.transform.Translate(Vector3.back * distance);
+        }
+
+        private void UpdatePreviousPosition(Vector3 position) =>
+            _previousPosition = position;
+
+        private Vector3 GetMousePoint() =>
+            camera.ScreenToViewportPoint(Input.mousePosition);
     }
-
-    private void RotateCamera(Vector3 direction) {
-        camera.transform.position = target.position;
-        camera.transform.Rotate(Vector3.right, direction.y * 180f * Time.deltaTime);
-        camera.transform.Rotate(Vector3.up, -direction.x * 180f * Time.deltaTime, Space.World);
-        camera.transform.Translate(Vector3.back * distance);
-    }
-
-    private void UpdatePreviousPosition(Vector3 position) =>
-        _previousPosition = position;
-
-    private Vector3 GetMousePoint() =>
-        camera.ScreenToViewportPoint(Input.mousePosition);
 }
