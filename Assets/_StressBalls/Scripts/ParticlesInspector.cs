db0c36ab5bd093800ae8f6df1712e914c0bd582f
Igor Silchenko <kildsforkids@gmail.com>
2022-12-08 01:38:07 +1000

    Removed unused statements

--- Assets/_StressBalls/Scripts/ParticlesInspector.cs
+++ Assets/_StressBalls/Scripts/ParticlesInspector.cs
@@ -21,9 +21,6 @@ namespace StressBalls {
         private Transform _marker;
         private bool _isUsed;
 
-        float[] norms;
-        int[] counts;
-
         private void Start() {
             CreateMarker();
         }
@@ -60,72 +57,6 @@ namespace StressBalls {
             }
         }
 
-        private void GetBatchInfo() {
-
-            var dc = _particle.Actor.GetConstraintsByType(Oni.ConstraintType.ShapeMatching) as ObiConstraints<ObiShapeMatchingConstraintsBatch>;
-            var sc = _particle.Solver.GetConstraintsByType(Oni.ConstraintType.ShapeMatching) as ObiConstraints<ObiShapeMatchingConstraintsBatch>;
-
-            if (_particle != null) {
-                if (dc != null && sc != null) {
-                    for (int j = 0; j < dc.batches.Count; ++j) {
-                        var batch = dc.batches[j];
-                        var solverBatch = sc.batches[j];
-
-                        for (int i = 0; i < batch.activeConstraintCount; i++) {
-                            // use rotation-invariant Frobeniums norm to get amount of deformation.
-                            int offset = _particle.Actor.solverBatchOffsets[(int)Oni.ConstraintType.ShapeMatching][j];
-
-                            // use frobenius norm to estimate deformation.
-                            float deformation = solverBatch.linearTransforms[offset + i].FrobeniusNorm() - 2;
-
-                            for (int k = 0; k < batch.numIndices[i]; ++k) {
-                                int p = batch.particleIndices[batch.firstIndex[i] + k];
-                                int or = _particle.Actor.solverIndices[p];
-
-                                //if (or == _particle.Index) {
-                                //    Debug.Log($"Found in {i} activeConstraint of {j} batch");
-                                //}
-                                if (_particle.Solver.invMasses[or] > 0) {
-                                    norms[p] += deformation;
-                                    counts[p]++;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                int particleIndex = _particle.BlueprintIndex;
-                if (counts[particleIndex] > 0) {
-                    float deformation = norms[particleIndex] / counts[particleIndex] * 20f + 0.5f;
-                    //float deformation = norms[particleIndex] / counts[particleIndex];
-                    //Color color = gradient.Evaluate(deformation);
-                    //Debug.Log($"<color=green>[{particleIndex}] {deformation}</color>");
-                    //Debug.Log(deformation.ToString("N2"));
-                }
-
-                //for (int i = 0; i < softbody.solverIndices.Length; ++i) {
-                //    if (counts[i] > 0) {
-                //        int solverIndex = softbody.solverIndices[i];
-                //        //softbody.solver.colors[solverIndex] = gradient.Evaluate(norms[i] / counts[i] * deformationScaling + 0.5f);
-                //        float deformation = norms[i] / counts[i] * 10f + 0.5f;
-                //        if (solverIndex == particleIndex) {
-                //            Debug.Log($"<color=green>[{solverIndex}] {deformation}</color>");
-
-                //        } else {
-                //            Debug.Log($"[{solverIndex}] {deformation}");
-                //        }
-                //        norms[i] = 0;
-                //        counts[i] = 0;
-                //    }
-                //}
-            }
-        }
-
-        private void Softbody_OnEndStep(ObiActor actor, float substepTime) {
-            if (_particle == null) return;
-            //GetBatchInfo();
-        }
-
         private void Pick() {
 
             float stiffness = (_particle.Actor as ObiSoftbody).deformationResistance;
@@ -151,14 +82,7 @@ namespace StressBalls {
 
         private void Picker_OnParticlePicked(ObiParticlePicker.ParticlePickEventArgs eventArgs) {
             if (!_isUsed) return;
-            if (_particle != null) {
-                _particle.Actor.OnEndStep -= Softbody_OnEndStep;
-            }
             _particle = new Particle(eventArgs.particleIndex, solver);
-            _particle.Actor.OnEndStep += Softbody_OnEndStep;
-
-            norms = new float[_particle.Actor.particleCount];
-            counts = new int[_particle.Actor.particleCount];
         }
 
         private void TurnOn() {
